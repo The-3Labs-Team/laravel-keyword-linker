0cca726348c7449d25c1d2a9f8f08a277d0d484c
murdercode <murdercode@users.noreply.github.com>
2024-07-02 14:13:58 +0000

    Fix styling

--- src/KeywordLinker.php
+++ src/KeywordLinker.php
@@ -2,16 +2,20 @@
 
 namespace The3LabsTeam\KeywordLinker;
 
-class KeywordLinker {
+class KeywordLinker
+{
     public string $content;
+
     public array $keywords;
 
-    public function __construct(string $content, array $keywords) {
+    public function __construct(string $content, array $keywords)
+    {
         $this->content = $content;
         $this->keywords = $keywords;
     }
 
-    public function parse(): string {
+    public function parse(): string
+    {
         foreach ($this->keywords as $keyword) {
             $this->content = preg_replace(
                 "/\b($keyword)\b/i",
