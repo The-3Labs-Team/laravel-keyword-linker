b971e9438ef978b7eba77baf8fcecef58d7e3424
Claudio-Emmolo <113107618+Claudio-Emmolo@users.noreply.github.com>
2024-07-03 17:04:21 +0200

    Add rel and target

--- src/KeywordLinker.php
+++ src/KeywordLinker.php
@@ -4,13 +4,26 @@ namespace The3LabsTeam\KeywordLinker;
 
 class KeywordLinker
 {
+    /**
+     * @param string $content
+     * @param array $keywords - ['keyword' => ['link' => 'url', 'rel' => 'nofollow', 'target' => '_blank']]
+     * @return string
+     */
     public function parse(string $content, array $keywords): string
     {
         $limit = config('keyword-linker.limit-auto-keywords') ?? -1;
-        foreach ($keywords as $keyword => $link) {
+
+
+        foreach ($keywords as $keyword => $data) {
+            $link = $data['url'];
+            $rel = $data['rel'] ? ' rel=\'' . $data['rel'] . '\'' : '';
+            $target = $data['target'] && $data['target'] !== '_self' ? ' target=\'' . $data['target'] . '\'' : '';
+
+            $replacement = "<a href='$link'$rel$target>$1</a>";
+
             $content = preg_replace(
                 "/\b($keyword)\b(?![^<]*>|[^<>]*<\/a>|[^[]*\])/i",
-                "<a href='$link'>$1</a>",
+                $replacement,
                 $content,
                 $limit
             );
--- tests/KeywordLinkerTest.php
+++ tests/KeywordLinkerTest.php
@@ -3,7 +3,61 @@
 use The3LabsTeam\KeywordLinker\Facades\KeywordLinker;
 
 beforeEach(function () {
-    $this->keywords = ['test' => 'https://example.com/test', 'example' => 'https://example.com/example'];
+    $this->keywords =
+        [
+            'test' => [
+                'url' => 'https://example.com/test',
+                'rel' => null,
+                'target' => null,
+            ],
+            'example' => [
+                'url' => 'https://example.com/example',
+                'rel' => null,
+                'target' => null,
+            ],
+        ];
+
+    $this->relKeywords =
+        [
+            'test' => [
+                'url' => 'https://example.com/test',
+                'rel' => 'nofollow',
+                'target' => null,
+            ],
+            'example' => [
+                'url' => 'https://example.com/example',
+                'rel' => 'sponsored',
+                'target' => null,
+            ],
+        ];
+
+    $this->targetKeywords =
+        [
+            'test' => [
+                'url' => 'https://example.com/test',
+                'rel' => null,
+                'target' => '_blank',
+            ],
+            'example' => [
+                'url' => 'https://example.com/example',
+                'rel' => null,
+                'target' => '_self',
+            ],
+        ];
+
+    $this->relTargetKeywords =
+        [
+            'test' => [
+                'url' => 'https://example.com/test',
+                'rel' => 'nofollow',
+                'target' => '_blank',
+            ],
+            'example' => [
+                'url' => 'https://example.com/example',
+                'rel' => 'sponsored',
+                'target' => '_self',
+            ],
+        ];
 });
 
 it('can add link to keyword', function () {
@@ -65,4 +119,39 @@ it('can add link to keyword without parsing into shortcodes', function () {
     $content = 'This is a [test]';
     $parsedContent = KeywordLinker::parse($content, $this->keywords);
     expect($parsedContent)->toBe('This is a [test]');
+});
+
+//link with rel attribute nofollow
+it('can add link to keyword with rel attribute nofollow', function () {
+    $content = 'This is a test';
+    $parsedContent = KeywordLinker::parse($content, $this->relKeywords);
+    expect($parsedContent)->toBe('This is a <a href=\'https://example.com/test\' rel=\'nofollow\'>test</a>');
+});
+
+//link with rel attribute sponsored
+it('can add link to keyword with rel attribute sponsored', function () {
+    $content = 'This is a example';
+    $parsedContent = KeywordLinker::parse($content, $this->relKeywords);
+    expect($parsedContent)->toBe('This is a <a href=\'https://example.com/example\' rel=\'sponsored\'>example</a>');
+});
+
+//link with target attribute _blank
+it('can add link to keyword with target attribute _blank', function () {
+    $content = 'This is a test';
+    $parsedContent = KeywordLinker::parse($content, $this->targetKeywords);
+    expect($parsedContent)->toBe('This is a <a href=\'https://example.com/test\' target=\'_blank\'>test</a>');
+});
+
+//link with target attribute _self
+it('can add link to keyword with target attribute _self', function () {
+    $content = 'This is a example';
+    $parsedContent = KeywordLinker::parse($content, $this->targetKeywords);
+    expect($parsedContent)->toBe('This is a <a href=\'https://example.com/example\'>example</a>');
+});
+
+//link with rel and target attribute
+it('can add link to keyword with rel and target attribute', function () {
+    $content = 'This is a test';
+    $parsedContent = KeywordLinker::parse($content, $this->relTargetKeywords);
+    expect($parsedContent)->toBe('This is a <a href=\'https://example.com/test\' rel=\'nofollow\' target=\'_blank\'>test</a>');
 });
